3066ccd11ed88b24d8866b6c564bae79f17b2f7b
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-11-16 15:13:05 +0530

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -33,22 +33,6 @@ async def help(bot, update):
     await update.reply_text(
         text=HELP_TEXT,
         disable_web_page_preview=True,
-        reply_markup=HELP_BUTTONS,
-        quote=True
-    )
-     
-
-HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
-        ]]
-    )
-
-@Client.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
-    await update.reply_text(
-        text=START_TEXT.format(update.from_user.mention),
         reply_markup=START_BUTTONS,
-        disable_web_page_preview=True,
         quote=True
     )
