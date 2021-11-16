a3e7b72459502a09b4fd68c68018d30acf4aa145
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-11-16 15:22:45 +0530

    Update start.py

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -14,14 +14,17 @@ START_TEXT = """
 >> `Sᴇɴᴅ ᴍᴇ Lɪɴᴋ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sʜᴏʀᴛᴇɴ`
 """
 
-HELP_TEXT = """**Hᴇʏ, Fᴏʟʟᴏᴡ ᴛʜᴇsᴇ sᴛᴇᴘs:**
-
-➠ Jᴜsᴛ sᴇɴᴅ ᴀ ʟɪɴᴋ ғᴏʀ sʜᴏʀᴛɪɴɢ.
-➠ I ᴡɪʟʟ sᴇɴᴅ ᴛʜᴇ sʜᴏʀᴛᴇᴅ ʟɪɴᴋs.
-"""
-
 START_BUTTONS = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('ᴍᴏᴠɪᴇ ɢʀᴏᴜᴘ', url='https://t.me/storytym')
         ]]
+    )
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
     )
